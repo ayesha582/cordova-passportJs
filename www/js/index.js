ecc772b56b7208bf5820084131440bb40fd17749
ayesha <ayeshamundu582@gmail.com>
2018-06-25 00:31:42 +0530

    facebook login working

--- www/js/index.js
+++ www/js/index.js
@@ -18,32 +18,34 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
 
-        $('.btn-submit').on('click',submitFunc);
+        $('.btn-submit').on('click', submitFunc);
 
-        function submitFunc(){
+        function submitFunc() {
 
-            $.get("https://cb02b2f2.ngrok.io/api/v1/auth/facebook", function(data, status){
-                alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-            });
+            // $.get("http://127.0.0.1:4500/api/v1/auth/facebook", function(data, status){
+            //     alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+            // }); 
 
-            // $.ajax({
-            //     url: "http://192.168.0.102/api/v1/auth/facebook",
-            //     type: "GET",
-            //     contentType: "application/json; charset=utf-8",
-            // }).done(function(response, textStatus, jqXHR) {
-            //     alert(JSON.stringify(response));
-            // }).fail(function(jqXHR, textStatus, errorThrown) {
-            //    alert(JSON.stringify(errorThrown));
-            // })
+            $.ajax({
+                url: "http://127.0.0.1:4500/api/v1/auth/facebook",
+                type: "GET",
+                crossDomain: true,
+                dataType: 'jsonp',
+                contentType: "application/json; charset=utf-8",
+            }).done(function (response, textStatus, jqXHR) {
+                alert(JSON.stringify(response));
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                alert(JSON.stringify(errorThrown));
+            })
             // var email = $(".email").val();
             // var password = $(".password").val();
             // alert(`attempt to login ${email} with password ${password}`);
         }
         // document.querySelector('button').addEventListener('click', this.clickHandler.bind(this), false);
-           //  main();
+        //  main();
     },
 
     // clickHandler: function(element) {
@@ -54,12 +56,12 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+    onDeviceReady: function () {
+        // this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
